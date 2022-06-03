14d6ead23036c65effba7b7d36c7d19b90df684e
ashleycma <ashley@aitken.io>
2022-06-03 19:48:54 +0100

    feat: Add userContext provider
    Add api proxy to package.json

---
+++ frontend/src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React, {createContext, useState} from 'react'
+
+export const UserContext = createContext(null)
+
+const {Provider} = UserContext
+
+const UserProvider = ({children}) => {
+  const [state, setState] = useState(null)
+  return <Provider value={[state, setState]}>{children}</Provider>
+}
+
+UserProvider.context = UserContext
+
+export default UserProvider
