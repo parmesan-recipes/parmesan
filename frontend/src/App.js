9b56505ee35922b713ee1f393e86cd5321c6cfc9
ashleycma <55878907+ashleycma@users.noreply.github.com>
2022-06-03 17:30:59 +0100

    feat: Add router to App.js (#13)
    * feat: Add router to App.js
    
    Add router library
    Add router to App.js
    Add home and 404 route
    Add testing for both routes
    
    * refactor: Update to newer version attributes
    
    * refactor: Change back to old attributes
    
    Also update to newest version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+
 import './App.scss'
 
 function App () {
-  return (<h1>Parmesan</h1>)
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<h1>Parmesan</h1>} />
+        <Route path='*' element={<h1>Error 404</h1>} />
+      </Routes>
+
+    </div>
+  )
 }
 
 export default App
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react'
+import { createMemoryHistory } from 'history'
+
 import App from './App'
+import { Router } from 'react-router-dom'
 
 test('Renders basic app', () => {
-  render(<App />)
+  const history = createMemoryHistory()
+  render(
+    <Router location={history.location} navigator={history}>
+      <App />
+    </Router>
+  )
   const linkElement = screen.getByText(/Parmesan/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('Tests 404 for unknown route', () => {
+  const history = createMemoryHistory()
+  history.push('/ksagf/aksjfhdsf/askjd')
+  render(
+    <Router location={history.location} navigator={history}>
+      <App />
+    </Router>
+  )
+  const linkElement = screen.getByText(/Error/i)
   expect(linkElement).toBeInTheDocument()
 })
