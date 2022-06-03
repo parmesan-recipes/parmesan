a4a8e2a5cdf9d9b08d38840f19bfd1c1382a6b10
Emily Maryon <29664150+EmilyMaryon@users.noreply.github.com>
2022-06-03 17:44:11 +0100

    fix: fixed express server code to serve files (#16)

--- backend/main.js
+++ backend/main.js
@@ -2,9 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.json())
-
 const port = 8080
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
