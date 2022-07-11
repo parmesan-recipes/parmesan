7a1bbb0d4590fe4f333a33d3256ac17ebf560421
ashleycma <ashley@aitken.io>
2022-07-11 11:03:53 +0100

    feat: Move login form to component
    Also add temporary logout button

---
+++ frontend/src/components/user/LoginForm.js
@@ -0,0 +1,52 @@
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { UserContext } from '../../context/UserContext.js'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+
+
+export function LoginForm() {
+  const navigate = useNavigate()
+  const [user, setUser] = useContext(UserContext)
+  
+  const submit = async (values, formik) => {
+    console.log(values)
+    
+    const res = await fetch(
+      '/api/v1/user/login',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(values)
+      })
+    
+    if (res.status === 200) {
+      const user = await res.json()
+      setUser(user)
+      navigate('/')
+    } else {
+      formik.setFieldError('username', 'Username or password is incorrect.')
+    }
+  }
+  
+  return (
+    <Formik initialValues={{ username: '', password: '' }} onSubmit={submit}>
+      
+      {({ isSubmitting }) => (
+        
+        <Form>
+          <label htmlFor='username'>Username</label>
+          <Field type='text' name='username' />
+          <ErrorMessage name='username' component='div' />
+          
+          <label htmlFor='password'>Password</label>
+          <Field type='password' name='password' />
+          <ErrorMessage name='password' component='div' />
+          <button type='submit' disabled={isSubmitting}>Submit</button>
+        
+        </Form>
+      
+      )}
+    
+    </Formik>
+  )
+}
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,50 +2,23 @@ import { ErrorMessage, Field, Form, Formik } from 'formik'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/UserContext.js'
 import { useContext } from 'react'
+import { LoginForm } from '../components/user/LoginForm.js'
 
 export function Login () {
-  const navigate = useNavigate()
   const [user, setUser] = useContext(UserContext)
-
-  const submit = async (values, formik) => {
-    console.log(values)
-
-    const res = await fetch(
-      '/api/v1/user/login',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(values)
-      })
-
-    if (res.status === 200) {
-      const user = await res.json()
-      setUser(user)
-      navigate('/')
-    } else {
-      formik.setFieldError('username', 'Username or password is incorrect.')
-    }
+  
+  const logout = () => {
+    fetch('/api/v1/user/logout', {method: 'post'})
+    setUser(undefined)
   }
-
-  return (
-    <Formik initialValues={{ username: '', password: '' }} onSubmit={submit}>
-
-      {({ isSubmitting }) => (
-
-        <Form>
-          <label htmlFor='username'>Username</label>
-          <Field type='text' name='username' />
-          <ErrorMessage name='username' component='div' />
-
-          <label htmlFor='password'>Password</label>
-          <Field type='password' name='password' />
-          <ErrorMessage name='password' component='div' />
-          <button type='submit' disabled={isSubmitting}>Submit</button>
-
-        </Form>
-
-      )}
-
-    </Formik>
-  )
+  
+  if (user) {
+    return (<div>
+      <h1>Logged in as {user.username}</h1>
+      <button onClick={logout}>Logout</button>
+    </div>)
+  } else {
+    return <LoginForm/>
+  }
+  
 }
