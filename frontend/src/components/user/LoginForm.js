9881ececf5c96bc4864d775c2e327e5b25f91507
ashleycma <ashley@aitken.io>
2022-07-11 11:04:42 +0100

    feat: Move login form to component
    Also add temporary logout button

--- frontend/src/components/user/LoginForm.js
+++ frontend/src/components/user/LoginForm.js
@@ -3,14 +3,13 @@ import { useContext } from 'react'
 import { UserContext } from '../../context/UserContext.js'
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 
-
-export function LoginForm() {
+export function LoginForm () {
   const navigate = useNavigate()
   const [user, setUser] = useContext(UserContext)
-  
+
   const submit = async (values, formik) => {
     console.log(values)
-    
+
     const res = await fetch(
       '/api/v1/user/login',
       {
@@ -18,7 +17,7 @@ export function LoginForm() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(values)
       })
-    
+
     if (res.status === 200) {
       const user = await res.json()
       setUser(user)
@@ -27,26 +26,26 @@ export function LoginForm() {
       formik.setFieldError('username', 'Username or password is incorrect.')
     }
   }
-  
+
   return (
     <Formik initialValues={{ username: '', password: '' }} onSubmit={submit}>
-      
+
       {({ isSubmitting }) => (
-        
+
         <Form>
           <label htmlFor='username'>Username</label>
           <Field type='text' name='username' />
           <ErrorMessage name='username' component='div' />
-          
+
           <label htmlFor='password'>Password</label>
           <Field type='password' name='password' />
           <ErrorMessage name='password' component='div' />
           <button type='submit' disabled={isSubmitting}>Submit</button>
-        
+
         </Form>
-      
+
       )}
-    
+
     </Formik>
   )
 }
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,24 +1,23 @@
-import { ErrorMessage, Field, Form, Formik } from 'formik'
-import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/UserContext.js'
 import { useContext } from 'react'
 import { LoginForm } from '../components/user/LoginForm.js'
 
 export function Login () {
   const [user, setUser] = useContext(UserContext)
-  
+
   const logout = () => {
-    fetch('/api/v1/user/logout', {method: 'post'})
+    fetch('/api/v1/user/logout', { method: 'post' })
     setUser(undefined)
   }
-  
+
   if (user) {
-    return (<div>
-      <h1>Logged in as {user.username}</h1>
-      <button onClick={logout}>Logout</button>
-    </div>)
+    return (
+      <div>
+        <h1>Logged in as {user.username}</h1>
+        <button onClick={logout}>Logout</button>
+      </div>
+    )
   } else {
-    return <LoginForm/>
+    return <LoginForm />
   }
-  
 }
