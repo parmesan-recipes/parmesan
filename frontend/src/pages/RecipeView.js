d34469f760dc3dcca41ccd1d70911ccf0d21ddd8
ashleycma <ashley@aitken.io>
2022-07-15 22:37:03 +0100

    fix: Remove test data and add fetch

--- frontend/src/pages/RecipeView.js
+++ frontend/src/pages/RecipeView.js
@@ -16,74 +16,18 @@ export function RecipeView () {
     setLoading(true)
     setRecipe(null)
 
-    //  Get from server
-
-    await new Promise(resolve => setTimeout(resolve, 100))
-
-    setRecipe({
-      recipeId,
-      name: 'Spaghetti Bolognese',
-      creationDate: 1657463823,
-      modificationDate: 1657386596,
-      tags: [{ name: 'spag', color: { r: 255, g: 78, b: 0 } }, { name: 'bol', color: { r: 0, g: 0, b: 0 } }],
-      time: {
-        prep: 15023,
-        cook: 121
-      },
-      images: [
-        { url: 'https://placekitten.com/300/200', alt: 'Spag bol finished with parmesan', caption: 'Finished spaghetti Bolognese topped with parmesan' },
-        { url: 'https://placekitten.com/300/201', alt: 'Spag bol in pot', caption: 'Finished spaghetti Bolognese in the pot' },
-        { url: 'https://placekitten.com/300/202', alt: 'Spag bol' }
-      ],
-      ingredientGroups: [
-        {
-          name: 'Bolognese',
-          ingredients: [
-            { name: 'Meat', amount: 500, unit: 'g' },
-            { name: 'Passata', amount: 500, unit: 'g' },
-            { name: 'carrot', amount: 2, unit: '' },
-            { name: 'pesto', amount: 3, unit: 'tsp' },
-            { name: 'oregano', amount: 3, unit: 'tsp' },
-            { name: 'stock cubes', amount: 2, unit: '' }
-          ]
-        },
-        {
-          name: 'spaghetti',
-          ingredients: [
-            { name: 'Spaghetti', amount: 100, unit: 'g' }
-          ]
-        }
-      ],
-      stepGroups: [
-        {
-          name: 'Vegetable prep',
-          steps: [
-            { text: 'Dice onions' },
-            { text: 'Crush garlic' },
-            { text: 'Grate carrot' }
-          ]
-        },
-        {
-          name: 'Bolognese',
-          steps: [
-            { text: 'Fry onions until they look right' },
-            { text: 'Add garlic to onions and fry for 5 minutes more' },
-            { text: 'Add carrot for 5 minutes' },
-            { text: 'Add minced meat and cook until it starts to turn grey' },
-            { text: 'Add passata, stock cubes, oregano and pesto and simmer until desired consistency, stir every five minutes' }
-          ]
-        },
-        {
-          name: 'Spaghetti',
-          steps: [
-            { text: 'Bring pan of water to boil' },
-            { text: 'Put in spaghetti and cook for 10 minutes' },
-            { text: 'Strain spaghetti in a sieve or colander' }
-          ]
-        }
-      ]
-    })
-    setLoading(false)
+    const recipeResponse = await fetch(`/api/v1/recipe/${recipeId}`)
+    if (recipeResponse.ok) {
+      recipeResponse.json().then(recipe => {
+        setRecipe(recipe)
+        setLoading(false)
+      }).catch(error => {
+        console.log(error)
+        setLoading(false)
+      })
+    } else {
+      setLoading(false)
+    }
   }, [recipeId])
 
   if (loading) {
